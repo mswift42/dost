7b89ef0df73873c4efcbc964096d1188aa96653f
mswift42 <martin.haesler@gmail.com>
2016-01-23 16:36:43 +0000

    add generated files.

---
+++ web/main.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library dost.web;
+
+import 'package:angular2/bootstrap.dart';
+
+import 'package:dost/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
