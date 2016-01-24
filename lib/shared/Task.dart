3e35cbe90208ad25e02c92d985be176b97fbce6f
mswift42 <martin.haesler@gmail.com>
2016-01-24 12:55:42 +0000

    add class TaskCategories.

--- lib/shared/Task.dart
+++ lib/shared/Task.dart
@@ -15,3 +15,22 @@ class Task {
     "Purchases"
   ]);
 }
+
+class TaskCategory {
+  String category;
+}
+
+class TaskCategories {
+  final Set<TaskCategory> categories = new Set<TaskCategory>.from([
+    "Work",
+    "Travel",
+    "Personal",
+    "Fitness",
+    "Friends",
+    "Family",
+    "Purchases"
+  ]);
+  void addCategory(TaskCategory cat) {
+    categories.add(cat);
+  }
+}
