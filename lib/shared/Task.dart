9631315415196f1bce226dbaa4aef70efcb0631e
mswift42 <martin.haesler@gmail.com>
2016-01-24 09:07:27 +0000

    add category set.

--- lib/shared/Task.dart
+++ lib/shared/Task.dart
@@ -1,15 +1,17 @@
 library Task;
 
-
-
 class Task {
   String summary;
   List<String> tasknotes;
   DateTime scheduled;
-  TaskCatergory category;
-
-}
-
-class TaskCatergory {
-static final Set<String> categories = new Set<String>.from(["work", "travel", "family", "private","purchases"]);
+  int id;
+  Set<String> category = new Set<String>.from([
+    "Work",
+    "Travel",
+    "Personal",
+    "Fitness",
+    "Friends",
+    "Family",
+    "Purchases"
+  ]);
 }
