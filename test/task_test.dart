9a9fd61ef39a5b6572eb6e68bb15534cd9c1bb65
mswift42 <martin.haesler@gmail.com>
2016-01-30 17:01:17 +0000

    add tasknotes adding / reomving test.

--- test/task_test.dart
+++ test/task_test.dart
@@ -17,4 +17,15 @@ void main() {
     t1.addCategory(new TaskCategory('hobby'));
     expect(t1.categories.length, 1);
   });
+  test('notes can be added to tasknotes', () {
+    var t1 = new Task('some summary');
+    expect(t1.tasknotes.length, 0);
+    t1.addTaskNote("first note");
+    expect(t1.tasknotes.length, 1);
+    t1.addTaskNote("second note");
+    expect(t1.tasknotes.length, 2);
+    expect(t1.tasknotes[0], "second note");
+    t1.tasknotes.remove("second note");
+    expect(t1.tasknotes[0], "first note");
+  });
 }
