d0b3db501cc8407fb1a39b6fbe34de549b875a07
mswift42 <martin.haesler@gmail.com>
2016-01-31 09:55:38 +0000

    Add TaskService file

---
+++ lib/shared/TaskService.dart
@@ -0,0 +1,13 @@
+library dost.shared.TaskService;
+
+import 'package:angular2/angular2.dart' show Injectable;
+import 'package:dost/shared/Task.dart';
+
+@Injectable
+class TaskService {
+  List<Task> tasklist;
+
+  void addTask(Task task) {
+    tasklist.insert(0, task);
+  }
+}
