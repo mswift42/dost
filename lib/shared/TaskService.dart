c54bc3ae34a72ccb4768b8ec945b1cbf6e35efdf
mswift42 <martin.haesler@gmail.com>
2016-01-31 09:58:28 +0000

    Add deleteTask method

--- lib/shared/TaskService.dart
+++ lib/shared/TaskService.dart
@@ -3,11 +3,15 @@ library dost.shared.TaskService;
 import 'package:angular2/angular2.dart' show Injectable;
 import 'package:dost/shared/Task.dart';
 
-@Injectable
+@Injectable()
 class TaskService {
   List<Task> tasklist;
 
   void addTask(Task task) {
     tasklist.insert(0, task);
+  }
+
+  void deleteTask(Task task) {
+    tasklist.remove(task);
   }
 }
