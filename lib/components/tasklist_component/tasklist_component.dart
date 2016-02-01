128686e1f78e18237b210d8a3dd60c53461a93c5
mswift42 <martin.haesler@gmail.com>
2016-02-01 17:39:14 +0000

    Import TaskService

--- lib/components/tasklist_component/tasklist_component.dart
+++ lib/components/tasklist_component/tasklist_component.dart
@@ -1,8 +1,11 @@
 library dost.components.tasklist_component;
 
 import 'package:angular2/angular2.dart';
+import 'package:dost/shared/Task.dart';
+import 'package:dost/shared/TaskService.dart';
 
-@Component(selector: 'tasklist')
-@View(templateUrl: 'tasklist-component.html')
+@Component(selector: 'tasklist',
+    templateUrl: 'tasklist-component.html',
+    providers: const [TaskService])
 
 class TaskListComponent {}
