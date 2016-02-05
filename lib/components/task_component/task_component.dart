0cfe3fca30ca2b5a21b603f7be32d4fa32ca6b3c
mswift42 <martin.haesler@gmail.com>
2016-02-05 17:56:11 +0000

    add summary inputs to component

--- lib/components/task_component/task_component.dart
+++ lib/components/task_component/task_component.dart
@@ -2,7 +2,9 @@ library dost.components.task_component;
 
 import 'package:angular2/angular2.dart';
 
-@Component(selector: 'task', templateUrl: 'task-component.html')
+@Component(selector: 'task', templateUrl: 'task-component.html',
+    inputs: const ['summary'])
 
 class TaskComponent {
+  String summary;
 }
