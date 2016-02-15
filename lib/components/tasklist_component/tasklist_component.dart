6157dc7fae8addd677e9f15f6e0bfb48bfd42c44
mswift42 <martin.haesler@gmail.com>
2016-02-15 15:37:17 +0000

    add css file for tasklist component.

--- lib/components/tasklist_component/tasklist_component.dart
+++ lib/components/tasklist_component/tasklist_component.dart
@@ -8,20 +8,21 @@ import 'package:dost/components/task_component/task_component.dart';
 @Component(
     selector: 'tasklist',
     templateUrl: 'tasklist-component.html',
+    styleUrls: const ['tasklist-component.css'],
     directives: const [NgFor, TaskComponent],
     providers: const [TaskService])
-class TaskListComponent implements OnInit {
-  List<Task> tasks;
-  final TaskService _taskService;
-  TaskListComponent(this._taskService);
+  class TaskListComponent implements OnInit {
+    List<Task> tasks;
+    final TaskService _taskService;
+    TaskListComponent(this._taskService);
 
-  ngOnInit() {
-    tasks = _taskService.getTasks();
-  }
+    ngOnInit() {
+      tasks = _taskService.getTasks();
+    }
 
-  void submitNewTask(String summary) {
-    if (summary.length > 0) {
-      tasks.insert(0, new Task(summary));
+    void submitNewTask(String summary) {
+      if (summary.length > 0) {
+        tasks.insert(0, new Task(summary));
+      }
     }
   }
-}
