dcc42530ceb5af972b3f025047241bff25ac477f
mswift42 <martin.haesler@gmail.com>
2016-02-15 14:08:47 +0000

    add task-component css

--- lib/components/task_component/task_component.dart
+++ lib/components/task_component/task_component.dart
@@ -2,9 +2,11 @@ library dost.components.task_component;
 
 import 'package:angular2/angular2.dart';
 
-@Component(selector: 'task', templateUrl: 'task-component.html',
+@Component(
+    selector: 'task',
+    templateUrl: 'task-component.html',
+    styleUrls: const ['task-component.css'],
     inputs: const ['summary'])
-
 class TaskComponent {
   String summary;
 }
