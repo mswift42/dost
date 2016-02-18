58710f2c688d6e8bde3b6b2cfb06dcc5d06f65a7
mswift42 <martin.haesler@gmail.com>
2016-02-18 17:28:28 +0000

    Fix tests to use id in constructor

--- test/task_test.dart
+++ test/task_test.dart
@@ -5,20 +5,21 @@ import 'package:dost/shared/Task.dart';
 
 void main() {
   test('Task gets initialised correctly', () {
-    var t1 = new Task('summary1');
+    var t1 = new Task('summary1', 1);
     expect(t1.summary, 'summary1');
+    expect(t1.id, 1);
     expect(t1.scheduled, null);
-    expect(t1.tasknotes.length,0);
+    expect(t1.tasknotes.length, 0);
     expect(t1.categories.length, 0);
   });
   test('new task categories can be added', () {
-    var t1 = new Task('some summary');
+    var t1 = new Task('some summary', 1);
     expect(t1.categories.length, 0);
     t1.addCategory(new TaskCategory('hobby'));
     expect(t1.categories.length, 1);
   });
   test('notes can be added to tasknotes', () {
-    var t1 = new Task('some summary');
+    var t1 = new Task('some summary', 1);
     expect(t1.tasknotes.length, 0);
     t1.addTaskNote("first note");
     expect(t1.tasknotes.length, 1);
