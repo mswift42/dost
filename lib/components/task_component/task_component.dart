4998aa9d0a74203f93d669da0cd28a639966beb4
mswift42 <martin.haesler@gmail.com>
2016-02-23 16:51:11 +0000

    Import tasknote component

--- lib/components/task_component/task_component.dart
+++ lib/components/task_component/task_component.dart
@@ -2,11 +2,14 @@ library dost.components.task_component;
 
 import 'package:angular2/angular2.dart';
 import 'package:dost/shared/TaskService.dart' show TaskService;
+import 'package:dost/components/tasknote_component/tasknote_component.dart'
+    show TaskNoteComponent;
 
 @Component(
     selector: 'task',
     templateUrl: 'task-component.html',
     styleUrls: const ['task-component.css'],
+    directives: const [NgFor, TaskNoteComponent],
     inputs: const ['summary', 'taskid'])
 class TaskComponent {
   String summary;
