7682002233dfa6588dbb2218788a60111e9aadb3
mswift42 <martin.haesler@gmail.com>
2016-03-06 16:17:06 +0000

    Import TaskService

--- lib/components/newnote-component/newnote_component.dart
+++ lib/components/newnote-component/newnote_component.dart
@@ -1,6 +1,7 @@
 library dost.components.newnote_component;
 
 import 'package:angular2/angular2.dart' show Component;
+import 'package:dost/shared/TaskService.dart' show TaskService;
 
 @Component(
     selector: 'new-note',
@@ -10,6 +11,9 @@ inputs: const ['taskid']
 class NewNoteComponent {
   String taskid;
   bool addingNote;
+  final TaskService _taskService;
+
+  NewNoteComponent(this._taskService);
 
   void toggleAddingNote() {
     addingNote = !addingNote;
