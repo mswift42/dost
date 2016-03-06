87881cdc5a7977cd9dd20dafa11c7d6ce28ed066
mswift42 <martin.haesler@gmail.com>
2016-03-06 18:14:56 +0000

    add toggle and add method

--- lib/components/tasknote_component/tasknote_component.dart
+++ lib/components/tasknote_component/tasknote_component.dart
@@ -1,3 +1,26 @@
 library dost.components.tasknote_component;
 
 import 'package:angular2/angular2.dart';
+import 'package:dost/shared/TaskService.dart' show TaskService;
+
+@Component(
+    selector: 'task-note',
+    templateUrl: 'tasknote-component.html',
+    inputs: const ['tasknote', 'taskid', 'index'])
+class TaskNoteComponent {
+  String tasknote;
+  String taskid;
+  String index;
+  bool editing = false;
+ final TaskService _taskService;
+
+  TaskNoteComponent(this._taskService);
+
+
+  void toggleEditing() {
+    editing = !editing;
+  }
+  void editNote(String note) {
+    _taskService.editNote(taskid, index, tasknote);
+  }
+}
