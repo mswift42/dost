82085010a89e4ad8020752227d936f86057295ac
mswift42 <martin.haesler@gmail.com>
2016-03-06 18:15:16 +0000

    import NewNote and TaskNote directives

--- lib/components/tasklist_component/tasklist_component.dart
+++ lib/components/tasklist_component/tasklist_component.dart
@@ -4,13 +4,26 @@ import 'package:angular2/angular2.dart';
 import 'package:dost/shared/Task.dart';
 import 'package:dost/shared/TaskService.dart';
 import 'package:dost/components/task_component/task_component.dart';
+import 'package:dost/components/tasknote_component/tasknote_component.dart';
+import 'package:dost/components/summary-component/summary_component.dart' show SummaryComponent;
+import 'package:dost/components/newnote-component/newnote_component.dart' show NewNoteComponent;
 
 @Component(
     selector: 'tasklist',
     templateUrl: 'tasklist-component.html',
-    styleUrls: const ['tasklist-component.css'],
-    directives: const [NgFor, TaskComponent],
-    providers: const [TaskService])
+    styleUrls: const [
+      'tasklist-component.css'
+    ],
+    directives: const [
+      NgFor,
+      TaskComponent,
+      TaskNoteComponent,
+      SummaryComponent,
+      NewNoteComponent
+    ],
+    providers: const [
+      TaskService
+    ])
 class TaskListComponent implements OnInit {
   List<Task> tasks;
   final TaskService _taskService;
@@ -22,7 +35,7 @@ class TaskListComponent implements OnInit {
 
   void submitNewTask(String summary) {
     if (summary.length > 0) {
-     _taskService.addTask(new Task(summary, 1234));
+      _taskService.addTask(new Task(summary, 1234));
     }
   }
 }
