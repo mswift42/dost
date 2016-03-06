f54b76c0ba1f4296911400e5c606e310e701c68a
mswift42 <martin.haesler@gmail.com>
2016-03-06 09:29:57 +0000

    Add bool editing and toggle method

--- lib/components/summary-component/summary_component.dart
+++ lib/components/summary-component/summary_component.dart
@@ -4,9 +4,14 @@ import 'package:angular2/angular2.dart';
 
 @Component(
     selector: 'task-summary',
-    templateUrl: 'summary-somponent.html',
+    templateUrl: 'summary-component.html',
     styleUrls: const ['summary-component.css'],
     inputs: const ['summary'])
 class SummaryComponent {
   String summary;
+  bool editing = false;
+
+  void toggleEditing() {
+    editing = !editing;
+  }
 }
