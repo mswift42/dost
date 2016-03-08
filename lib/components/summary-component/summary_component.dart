303603d6c1fad99c12b29b767734ab8755658908
mswift42 <martin.haesler@gmail.com>
2016-03-08 14:48:44 +0000

    add taskid to inputs

--- lib/components/summary-component/summary_component.dart
+++ lib/components/summary-component/summary_component.dart
@@ -1,15 +1,20 @@
 library dost.components.summary_component;
 
 import 'package:angular2/angular2.dart';
+import 'package:dost/shared/TaskService.dart';
 
 @Component(
     selector: 'task-summary',
     templateUrl: 'summary-component.html',
     styleUrls: const ['summary-component.css'],
-    inputs: const ['summary'])
+    inputs: const ['summary', 'taskid'])
 class SummaryComponent {
   String summary;
   bool editing = false;
+  String taskid;
+  final TaskService _taskService;
+
+  SummaryComponent(this._taskService);
 
   void toggleEditing() {
     editing = !editing;
