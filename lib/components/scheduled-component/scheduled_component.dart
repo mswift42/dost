a913a61ce880d405c2fd4a6a87667eff756c36ad
mswift42 <martin.haesler@gmail.com>
2016-03-09 17:22:45 +0000

    import taskService.

--- lib/components/scheduled-component/scheduled_component.dart
+++ lib/components/scheduled-component/scheduled_component.dart
@@ -1,4 +1,5 @@
 import 'package:angular2/angular2.dart' show Component;
+import 'package:dost/shared/TaskService.dart' show TaskService;
 
 @Component(
     selector: 'scheduled',
@@ -8,4 +9,14 @@ import 'package:angular2/angular2.dart' show Component;
 class ScheduledComponent {
   DateTime scheduled;
   String taskid;
+  bool editing;
+  final TaskService _taskService;
+
+  ScheduledComponent(this._taskService);
+
+  void editScheduled(String scheduled) {
+    List<int> sched = scheduled.split("/").map((i) => int.parse(i));
+    _taskService.editScheduled(
+        taskid, new DateTime(sched[2], sched[1], sched[0]));
+  }
 }
