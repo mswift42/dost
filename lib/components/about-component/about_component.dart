5d273087953c4516058ea3cbf992ba79a0c1fb63
mswift42 <martin.haesler@gmail.com>
2016-03-28 13:11:25 +0100

    Add about component

---
+++ lib/components/about-component/about_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular2/core.dart' show Component;
+
+@Component(
+    selector: 'about-component',
+templateUrl: 'about-component.html',
+styleUrls: const ['about-component.css']
+)
+
+class AboutComponent {}
+
