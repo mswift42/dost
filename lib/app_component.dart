6b1b389a9e078d6df762eaa01519e2cca1e0ee5c
mswift42 <martin.haesler@gmail.com>
2016-03-28 13:14:15 +0100

    add router

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,11 +3,22 @@
 
 library dost.app_component;
 
-import 'package:angular2/angular2.dart';
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 import 'package:dost/components/tasklist_component/tasklist_component.dart';
+import 'package:dost/components/about-component/about_component.dart';
 
 @Component(
     selector: 'my-app',
     templateUrl: 'app_component.html',
-    directives: const [TaskListComponent])
+    directives: const [TaskListComponent, ROUTER_DIRECTIVES],
+    providers: const [ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Route(
+      path: '/tasks',
+      name: 'Tasklist',
+      component: TaskListComponent,
+      useAsDefault: true),
+  const Route(path: '/about', name: 'About', component: AboutComponent)
+])
 class AppComponent {}
